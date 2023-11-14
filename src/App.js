fb72119ad9edbf3516b4b47821391a1ad2f4d5b3
hemaher0 <hemahero@naver.com>
2023-11-14 23:07:06 +0900

    강민우 [11/14]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import Sidebar from "./components/left-side/Sidebar";
 import ChatScreen from "./components/main/ChatScreen";
 import Header from "./components/top-side/Header";
+import Data from "./components/details/Data";
+import {
+  BrowserRouter as Router ,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="w-full h-screen bg-gray-50 dark:bg-gray-900 flex flex-col">
+    <Router>
       <Header />
-      <div className="flex flex-grow mt-16">
-        <Sidebar />
-        <ChatScreen />
-      </div>
-    </div>
+      <Routes>
+        <Route path="/chat">
+          {/* <div className="w-full h-screen bg-gray-50 dark:bg-gray-900 flex flex-col"> 
+            <div className="flex flex-grow mt-16">
+              <Sidebar />
+              <ChatScreen />
+            </div>
+          </div> */}
+        </Route>
+        <Route path="/dashboard">
+            
+        </Route>
+        <Route path="/data" element={<Data />} />
+
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
