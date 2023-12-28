dae35b2f443486c8c63c2e756a3d9377b3bec7ca
hemaher0 <hemahero@naver.com>
2023-12-28 14:41:39 +0900

    [강민우] Sidebar, File Input Button modified

--- src/components/left-side/Sidebar.js
+++ src/components/left-side/Sidebar.js
@@ -1,8 +1,9 @@
-import { useSelector } from "react-redux";
-import React, { useState, useEffect } from "react";
-import PrintFileCards from "./PrintFileCards";
+import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import PrintFileCards from './PrintFileCards';
+import DataSelect from '../Data/DataSelect';
 
-function Sidebar({ page }) {
+function Sidebar({ page, jsonData }) {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [width, setWidth] = useState(300);
 
@@ -12,10 +13,10 @@ function Sidebar({ page }) {
     };
 
     // Attach window resize listener
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
     // Cleanup function to remove the event listener
-    return () => window.removeEventListener("resize", handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   const minWidth = 300;
@@ -33,9 +34,9 @@ function Sidebar({ page }) {
       setWidth(newWidth);
     };
 
-    document.addEventListener("mousemove", doDrag);
-    document.addEventListener("mouseup", () => {
-      document.removeEventListener("mousemove", doDrag);
+    document.addEventListener('mousemove', doDrag);
+    document.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', doDrag);
     });
   };
 
@@ -47,13 +48,13 @@ function Sidebar({ page }) {
       >
         <div>
           {page === 0 && <PrintFileCards />}
-          {/* {page === 1 && <Dashboard />}
-          {page === 2 && <Data />} */}
+          {page === 1 && <DataSelect jsonData={jsonData} />}
+          {page === 2 && <DataSelect jsonData={jsonData} />}
         </div>
       </aside>
       <div
         className="cursor-col-resize"
-        style={{ width: "5px", cursor: "col-resize" }}
+        style={{ width: '5px', cursor: 'col-resize' }}
         onMouseDown={handleMouseDown}
       ></div>
     </div>
--- src/components/main/Input/FileInputButton.js
+++ src/components/main/Input/FileInputButton.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { setSelectedFile } from "../../../reducers/chatScreenReducers";
-import { updateAppState } from "../../../reducers/appStateReducer";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setSelectedFile } from '../../../reducers/chatScreenReducers';
+import { updateAppState } from '../../../reducers/appStateReducer';
 
 // file upload 버튼 컴포넌트 return.
 // 파일 업로드 버튼 클릭 시, file input 창을 띄운다.
-function FileInput() {
-  const fileInput = React.createRef();
+function FileInputButton({ onFileChange }) {
+  const fileInput = useRef(null);
 
   // dispatch func
   const dispatch = useDispatch();
@@ -18,8 +18,12 @@ function FileInput() {
 
   // 파일 업로드 시
   const handleFileChange = (e) => {
-    dispatch(setSelectedFile(e.target.files[0]));
-    dispatch(updateAppState("file_uploading"));
+    const file = e.target.files[0];
+    if (file) {
+      dispatch(setSelectedFile(file));
+      dispatch(updateAppState('file_uploading'));
+      onFileChange(file);
+    }
   };
 
   return (
@@ -28,7 +32,7 @@ function FileInput() {
         type="file"
         onChange={handleFileChange}
         ref={fileInput}
-        style={{ display: "none" }}
+        style={{ display: 'none' }}
       />
       <button
         className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
@@ -40,4 +44,4 @@ function FileInput() {
   );
 }
 
-export default FileInput;
+export default FileInputButton;
